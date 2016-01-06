f4a503d20ec02da8fb994f8d4ad8dd8886357097
Ryan Eggert <ryaneggertwi@gmail.com>
2016-01-05 19:28:24 -0600

    Added gator_msgs/GlobalPose.

--- master_ws/devel/share/common-lisp/ros/gator_msgs/msg/_package.lisp
+++ master_ws/devel/share/common-lisp/ros/gator_msgs/msg/_package.lisp
@@ -1,6 +1,8 @@
 (cl:defpackage gator_msgs-msg
   (:use )
   (:export
+   "<GLOBALPOSE>"
+   "GLOBALPOSE"
    "<DRIVESTATE>"
    "DRIVESTATE"
   ))
---
+++ master_ws/devel/share/common-lisp/ros/gator_msgs/msg/_package_GlobalPose.lisp
@@ -0,0 +1,16 @@
+(cl:in-package gator_msgs-msg)
+(cl:export '(HEADER-VAL
+          HEADER
+          LATITUDE-VAL
+          LATITUDE
+          LONGITUDE-VAL
+          LONGITUDE
+          ALTITUDE-VAL
+          ALTITUDE
+          ROLL-VAL
+          ROLL
+          PITCH-VAL
+          PITCH
+          HEADING-VAL
+          HEADING
+))
