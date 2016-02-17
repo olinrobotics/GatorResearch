c4fc21cd16a51789bc3af747d1ff482ef8a3218c
Autonomous Gator <gatorresearch@gmail.com>
2016-02-16 22:07:21 -0500

    Changing UDP validation value.

--- master_ws/src/gator_communication/scripts/constants.py
+++ master_ws/src/gator_communication/scripts/constants.py
@@ -1,3 +1,3 @@
-# Change this to set the value and type prefixed to all outgoing UDP packets. 
-trans_prefix = 0x671E78E6
-trans_type = 'I' # Unsigned integer
+# Change this to set the value and type prefixed to all outgoing UDP packets.
+trans_prefix = 0x850D
+trans_type = 'H'  # Unsigned integer
