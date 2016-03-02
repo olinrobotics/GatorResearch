363c7aaf2148c5be6884e9a60a13dbaa505aab5c
Autonomous Gator <gatorresearch@gmail.com>
2016-03-01 20:28:48 -0500

    RE: Adding IMU reader

---
+++ master_ws/src/gator_communication/scripts/read_imu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# read_steer_angle.py
+
+from simple_udp import TopicPublisher
+from sensor_msgs.msg import Imu
+import rospy
+
+
+def main():
+    port = 5020
+    name = 'IMU'
+    rate = 30  # Hz
+    rospy.init_node('read_imu', anonymous=False)
+    publisher = rospy.Publisher(name, Imu, queue_size=5)
+    heartbeat = TopicPublisher(port, name, publisher, rate)
+    heartbeat.go()
+
+if __name__ == '__main__':
+    main()
