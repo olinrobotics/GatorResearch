4e2de1891c219f43b7817ec18262179572119615
RyanEggert <ryan.eggert@students.olin.edu>
2016-03-10 10:38:20 -0500

    Incremental improvements to wheel odometry. Work in progress.

--- master_ws/src/gator_odom/scripts/wheel_odom.py
+++ master_ws/src/gator_odom/scripts/wheel_odom.py
@@ -2,7 +2,10 @@
 
 import rospy
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Quaternion
 from std_msgs.msg import Float32, Float64
+
+from math import radians, sin, cos, tan
 
 
 def publish_on_new(func):
@@ -17,15 +20,16 @@ def publish_on_new(func):
 
 
 class WheelOdometryNode(object):
+
     """docstring for WheelOdometryNode"""
 
-    def __init__(self, pub_odom, wheel_speed_topic, wheel_speed_type,
-                 steer_angle_topic, steer_angle_type):
+    def __init__(self, pub_odom, wheel_dist_topic, wheel_dist_type,
+                 steer_angle_topic, steer_angle_type, frame_id, child_frame_id):
         super(WheelOdometryNode, self).__init__()
         self.pub = pub_odom
         self.wheel_sub = rospy.Subscriber(
-            wheel_speed_topic,
-            wheel_speed_type,
+            wheel_dist_topic,
+            wheel_dist_type,
             self.cb_wheel
         )
         self.angle_sub = rospy.Subscriber(
@@ -33,8 +37,17 @@ class WheelOdometryNode(object):
             steer_angle_type,
             self.cb_steer
         )
+
+        self.frame_id = frame_id
+        self.child_frame_id = child_frame_id
+
         self.wheel_data = None
         self.steer_data = None
+
+        # Assume starting at the origin & vehicle facing +x direction
+        self.x_est = 0
+        self.y_est = 0
+        self.theta_est = 0  # Theta measured from +x axis towards +y axis
 
         self.NEW_DATA_PAIR = False
 
@@ -45,6 +58,28 @@ class WheelOdometryNode(object):
     @publish_on_new
     def cb_steer(self, data):
         self.steer_data = data
+
+    def compute_odom(self):
+        L = rospy.get_param('vehicle_length')  # Len from rear diff to fr. axle
+        steer_rad = radians(self.steer_data)
+        d_dr = self.wheel_data
+        d_theta = d_dr/float(L) * tan(steer_rad)
+        dx = d_dr * cos(self.theta_est + d_theta/2)
+        dy = d_dr * sin(self.theta_est + d_theta/2)
+        self.x_est += dx
+        self.y_est += dy
+        self.theta_est += d_theta
+
+    def publish_odom_msg(self):
+        self.compute_odom()
+        msg = Odometry()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = self.frame_id
+        msg.child_frame_id = self.child_frame_id
+        msg.pose.pose.position = Point(self.x_est, self.y_est, 0)  # z = 0
+        msg.pose.pose.orientation = Quaternion()
+
+        self.pub.publish()
 
 
 def main():
